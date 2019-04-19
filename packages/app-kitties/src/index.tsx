0419271b8ab23376475a3dca047b40db5ce3e3f0
Bryan Chen <xlchen1291@gmail.com>
2019-04-19 22:09:18 +1200

    Able to create new kitty

--- packages/app-kitties/src/index.tsx
+++ packages/app-kitties/src/index.tsx
@@ -15,6 +15,7 @@ import React from 'react';
 
 // local imports and components
 import AccountSelector from './AccountSelector';
+import KittyActions from './KittyActions';
 import translate from './translate';
 import KittyViewer from './KittyViewer';
 
@@ -39,6 +40,7 @@ class App extends React.PureComponent<Props, State> {
       // and margins inside the application. (Just from a consistent pov)
       <main>
         <AccountSelector onChange={this.onAccountChange} />
+        <KittyActions accountId={accountId} />
         <KittyViewer/>
       </main>
     );
