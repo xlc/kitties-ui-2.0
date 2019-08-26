xlc/kitties-ui-old

Able to show kittiy count

packages/app-kitties/src/KittyViewer.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import BN from 'bn.js';
+import { withCalls } from '@polkadot/ui-api/with';
 
 import { Kitty } from './types';
 import KittyAvatar from './KittyAvatar';
 
 const Wrapper = styled.section``;
 
-const KittyViewer = ({ kitties }: { kitties: Array<Kitty> }) => (
-  <Wrapper>
-    <h1>Substrate Kitties</h1>
-    { kitties.length === 0 && 'No kitties'}
-    { kitties.map(k => (
-      <KittyAvatar dna={k.dna} />
-    )) }
-  </Wrapper>
-);
+type Props = {
+  kitties_kittiesCount: BN
+};
+type State = {};
 
-export default KittyViewer;
+class KittyViewer extends React.PureComponent<Props, State>{
+
+  render () {
+    const { kitties_kittiesCount: count } = this.props;
+    return (
+      <Wrapper>
+        <h1>Substrate Kitties</h1>
+        {/* { kitties.length === 0 && 'No kitties'}
+        { kitties.map(k => (
+          <KittyAvatar dna={k.dna} />
+        )) } */}
+        kitty count: {count && count.toString()}
+      </Wrapper>
+    );
+  }
+}
+
+export default withCalls<Props>(
+  'query.kitties.kittiesCount'
+)(KittyViewer);

xlc/kitties-ui-2.0

packages/app-kitties/src/KittyViewer.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import BN from 'bn.js';
+import { withCalls } from '@polkadot/react-api/with';
 
 import { Kitty } from './types';
 import KittyAvatar from './KittyAvatar';
 
 const Wrapper = styled.section``;
 
-const KittyViewer = ({ kitties }: { kitties: Array<Kitty> }) => (
-  <Wrapper>
-    <h1>Substrate Kitties</h1>
-    { kitties.length === 0 && 'No kitties'}
-    { kitties.map(k => (
-      <KittyAvatar dna={k.dna} />
-    )) }
-  </Wrapper>
-);
+type Props = {
+  kitties_kittiesCount: BN
+};
+type State = {};
 
-export default KittyViewer;
+class KittyViewer extends React.PureComponent<Props, State>{
+
+  render () {
+    const { kitties_kittiesCount: count } = this.props;
+    return (
+      <Wrapper>
+        <h1>Substrate Kitties</h1>
+        {/* { kitties.length === 0 && 'No kitties'}
+        { kitties.map(k => (
+          <KittyAvatar dna={k.dna} />
+        )) } */}
+        kitty count: {count && count.toString()}
+      </Wrapper>
+    );
+  }
+}
+
+export default withCalls<Props>(
+  'query.kitties.kittiesCount'
+)(KittyViewer);
9495ee8ce19f51f19f79c8f906990e1593a28327
Bryan Chen <xlchen1291@gmail.com>
2019-04-19 21:40:06 +1200

    Able to show kittiy count

--- packages/app-kitties/src/KittyViewer.tsx
+++ packages/app-kitties/src/KittyViewer.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import BN from 'bn.js';
+import { withCalls } from '@polkadot/react-api/with';
 
 import { Kitty } from './types';
 import KittyAvatar from './KittyAvatar';
 
 const Wrapper = styled.section``;
 
-const KittyViewer = ({ kitties }: { kitties: Array<Kitty> }) => (
-  <Wrapper>
-    <h1>Substrate Kitties</h1>
-    { kitties.length === 0 && 'No kitties'}
-    { kitties.map(k => (
-      <KittyAvatar dna={k.dna} />
-    )) }
-  </Wrapper>
-);
+type Props = {
+  kitties_kittiesCount: BN
+};
+type State = {};
 
-export default KittyViewer;
+class KittyViewer extends React.PureComponent<Props, State>{
+
+  render () {
+    const { kitties_kittiesCount: count } = this.props;
+    return (
+      <Wrapper>
+        <h1>Substrate Kitties</h1>
+        {/* { kitties.length === 0 && 'No kitties'}
+        { kitties.map(k => (
+          <KittyAvatar dna={k.dna} />
+        )) } */}
+        kitty count: {count && count.toString()}
+      </Wrapper>
+    );
+  }
+}
+
+export default withCalls<Props>(
+  'query.kitties.kittiesCount'
+)(KittyViewer);
--- packages/app-kitties/src/index.tsx
+++ packages/app-kitties/src/index.tsx
@@ -22,8 +22,6 @@ import * as types from './types';
 
 getTypeRegistry().register(types);
 
-const Kitty = types.Kitty;
-
 // define out internal types
 type Props = AppProps & I18nProps;
 interface State {
@@ -36,14 +34,12 @@ class App extends React.PureComponent<Props, State> {
   public render (): React.ReactNode {
     const { accountId } = this.state;
 
-    const kitties = [ new Kitty(), new Kitty(11111), new Kitty(222222222) ];
-
     return (
       // in all apps, the main wrapper is setup to allow the padding
       // and margins inside the application. (Just from a consistent pov)
       <main>
         <AccountSelector onChange={this.onAccountChange} />
-        <KittyViewer kitties={kitties} />
+        <KittyViewer/>
       </main>
     );
   }
