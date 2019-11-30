xlc/kitties-ui-old

Able to set kitty price

packages/app-kitties/src/KittyActions.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import BN from 'bn.js';
-import { Button, TxButton, InputNumber, InputAddress } from '@polkadot/ui-app';
+import { Button, TxButton, InputNumber, InputAddress, InputBalance } from '@polkadot/ui-app';
 
 import { KittyIndex } from './types';
 
@@ -15,7 +15,9 @@ type State = {
   parentKittyId1?: KittyIndex,
   parentKittyId2?: KittyIndex,
   recipientId?: string,
-  transferKittyId?: KittyIndex
+  transferKittyId?: KittyIndex,
+  sellKittyId?: KittyIndex,
+  sellKittyPrice?: BN
 };
 
 export default class KittyActions extends React.PureComponent<Props> {
@@ -41,9 +43,21 @@ export default class KittyActions extends React.PureComponent<Props> {
     this.setState({ transferKittyId: transferKittyId && new KittyIndex(transferKittyId) });
   }
 
+  private onSetSellKittyId = (sellKittyId?: BN) => {
+    this.setState({ sellKittyId: sellKittyId && new KittyIndex(sellKittyId) });
+  }
+
+  private onSetSellKittyPrice = (sellKittyPrice?: BN) => {
+    this.setState({ sellKittyPrice });
+  }
+
   render () {
     const { accountId } = this.props;
-    const { parentKittyId1, parentKittyId2, recipientId, transferKittyId } = this.state;
+    const {
+      parentKittyId1, parentKittyId2,
+      recipientId, transferKittyId,
+      sellKittyId, sellKittyPrice
+    } = this.state;
 
     return (
       <section>
@@ -99,6 +113,27 @@ export default class KittyActions extends React.PureComponent<Props> {
                 label='Transfer Kitty'
                 params={[recipientId, transferKittyId]}
                 tx='kitties.transfer'
+              />
+            </Button.Group>
+          </div>
+        </div>
+        <h2>Sell Kitty</h2>
+        <div className='ui--row'>
+          <div className='large'>
+            <InputNumber
+              label='Kitty ID to sell'
+              onChange={this.onSetSellKittyId}
+            />
+            <InputBalance
+              label='Kitty Price to sell'
+              onChange={this.onSetSellKittyPrice}
+            />
+            <Button.Group>
+              <TxButton
+                accountId={accountId}
+                label='Set Kitty Price'
+                params={[sellKittyId, sellKittyPrice]}
+                tx='kitties.ask'
               />
             </Button.Group>
           </div>

xlc/kitties-ui-2.0

packages/app-kitties/src/KittyActions.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import BN from 'bn.js';
-import { Button, TxButton, InputNumber, InputAddress } from '@polkadot/react-components';
+import { Button, TxButton, InputNumber, InputAddress, InputBalance } from '@polkadot/react-components';
 
 import { KittyIndex } from './types';
 
@@ -15,7 +15,9 @@ type State = {
   parentKittyId1?: KittyIndex,
   parentKittyId2?: KittyIndex,
   recipientId?: string,
-  transferKittyId?: KittyIndex
+  transferKittyId?: KittyIndex,
+  sellKittyId?: KittyIndex,
+  sellKittyPrice?: BN
 };
 
 export default class KittyActions extends React.PureComponent<Props> {
@@ -41,9 +43,21 @@ export default class KittyActions extends React.PureComponent<Props> {
     this.setState({ transferKittyId: transferKittyId && new KittyIndex(transferKittyId) });
   }
 
+  private onSetSellKittyId = (sellKittyId?: BN) => {
+    this.setState({ sellKittyId: sellKittyId && new KittyIndex(sellKittyId) });
+  }
+
+  private onSetSellKittyPrice = (sellKittyPrice?: BN) => {
+    this.setState({ sellKittyPrice });
+  }
+
   render () {
     const { accountId } = this.props;
-    const { parentKittyId1, parentKittyId2, recipientId, transferKittyId } = this.state;
+    const {
+      parentKittyId1, parentKittyId2,
+      recipientId, transferKittyId,
+      sellKittyId, sellKittyPrice
+    } = this.state;
 
     return (
       <section>
@@ -99,6 +113,27 @@ export default class KittyActions extends React.PureComponent<Props> {
                 label='Transfer Kitty'
                 params={[recipientId, transferKittyId]}
                 tx='kitties.transfer'
+              />
+            </Button.Group>
+          </div>
+        </div>
+        <h2>Sell Kitty</h2>
+        <div className='ui--row'>
+          <div className='large'>
+            <InputNumber
+              label='Kitty ID to sell'
+              onChange={this.onSetSellKittyId}
+            />
+            <InputBalance
+              label='Kitty Price to sell'
+              onChange={this.onSetSellKittyPrice}
+            />
+            <Button.Group>
+              <TxButton
+                accountId={accountId}
+                label='Set Kitty Price'
+                params={[sellKittyId, sellKittyPrice]}
+                tx='kitties.ask'
               />
             </Button.Group>
           </div>