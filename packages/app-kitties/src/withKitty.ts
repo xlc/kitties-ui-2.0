xlc/kitties-ui-old

Able to view kitties

packages/app-kitties/src/withKitty.ts
@@ -1,0 +1,11 @@
+import BN from 'bn.js';
+import { Option } from '@polkadot/types/codec';
+import { withCalls } from '@polkadot/ui-api/with';
+import { Kitty } from './types';
+
+export type Props = {
+  kittyId: BN,
+  kitties_kitties?: Option<Kitty>
+};
+
+export default withCalls<Props>(['query.kitties.kitties', { paramName: 'kittyId' }]);

xlc/kitties-ui-2.0

packages/app-kitties/src/withKitty.ts
@@ -1,0 +1,11 @@
+import BN from 'bn.js';
+import { Option } from '@polkadot/types/codec';
+import { withCalls } from '@polkadot/react-api/with';
+import { Kitty } from './types';
+
+export type Props = {
+  kittyId: BN,
+  kitties_kitties?: Option<Kitty>
+};
+
+export default withCalls<Props>(['query.kitties.kitties', { paramName: 'kittyId' }]);